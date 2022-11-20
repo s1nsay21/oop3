f355dc78bc6842932bb5d1a09bfe233ce6bbdb27
s1nsay21 <112234777+s1nsay21@users.noreply.github.com>
2022-11-20 12:36:41 +0200

    Add files via upload
    first commit

---
+++ CP.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3Oop.o.tichshenkooop.assignment1
+{
+    public abstract class ColorPrinter{
+
+        public abstract void printInColor(String value);
+    }
+}
